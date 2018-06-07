9cc8239c112465664eea339518b75619ae929280
quoyle <quoyle@163.com>
2018-06-07 17:33:37 +0800

    添加.gitignore

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop alpine
-docker rm alpine
-docker rmi jfuwu/alpine 
+docker rmi -f jfuwu/alpine 
 
 # 重新生成
 docker build -t jfuwu/alpine ./
--- php71/build.sh
+++ php71/build.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # 清除已有的
-docker stop php71
-docker rm php71
-docker rmi jfuwu/php71
+docker rmi -f jfuwu/php71
 
 # 重新生成
 docker build -t jfuwu/php71 ./
